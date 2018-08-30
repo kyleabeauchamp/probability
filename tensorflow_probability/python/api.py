67bed7703f22f11623c0d37f872727af424f1b66
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2018-08-29 21:57:10 -0700

    Fix docstring

--- tensorflow_probability/python/api.py
+++ tensorflow_probability/python/api.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Monte Carlo integration and helpers."""
+"""Higher level API for Bayesian Inference."""
 
 from __future__ import absolute_import
 from __future__ import division
